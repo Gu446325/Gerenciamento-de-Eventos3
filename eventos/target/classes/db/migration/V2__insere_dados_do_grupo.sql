262d40fcda627ecb4f0622a0a77f14edd4445f68
Luiz <luiz.trisoft@gmail.com>
2017-02-26 12:20:04 -0400

    Cleaning

--- eventos/target/classes/db/migration/V2__insere_dados_do_grupo.sql
+++
@@ -1,8 +0,0 @@
-insert into grupo (id, nome, descricao)
-select 1, 'PARTICIPANTES', 'Alunos' WHERE NOT EXISTS (SELECT 1 FROM grupo WHERE id = 1);
-
-insert into grupo (id, nome, descricao)
-select 2, 'EMPRESAS', 'Cria eventos' WHERE NOT EXISTS (SELECT 1 FROM grupo WHERE id = 2);
-
-insert into grupo (id, nome, descricao)
-select 3, 'ADMINISTRADORES', 'Gerencia' WHERE NOT EXISTS (SELECT 1 FROM grupo WHERE id = 3);
